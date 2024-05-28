e1b08271214ac36cff15f37c2515d075aa020e3e
VerkhovtsovDenis <verkhovtcov.do@students.dvfu.ru>
2024-05-28 18:17:54 +1000

    лаба 4

---
+++ Модуль 2. JavaScript/ЛР 4. Библиотеки JavaScript, D3/lab_4.1/program.js
@@ -0,0 +1,431 @@
+body = d3.select("body");
+
+svg = d3.select("svg");
+
+class Draw {
+    constructor(svg, centerX, centerY) {
+        this.svg = svg;
+        this.centerX = centerX;
+        this.centerY = centerY;
+    }
+
+    draw() {
+        // To be implemented by subclasses
+    }
+
+    animate(duration) {
+		console.log(123)
+
+        this.svg
+            .transition()
+            .duration(duration * 0.2)
+            .attr("transform", `translate(${+200}, ${0})`)
+
+		this.centerX = this.centerX + 200
+
+        this.svg
+            .transition()
+            .duration(duration * 0.3)
+            .attr("transform", `translate(${100}, ${-100})`)
+
+        this.svg
+            .transition()
+            .duration(duration * 0.3)
+            .attr("transform", `translate(${-100}, ${-100})`)
+
+        this.svg
+            .transition()
+            .duration(duration * 0.2)
+            .attr("transform", `translate(${-200}, ${0})`)
+
+    }
+}
+
+class Raccoon extends Draw {
+    draw() {
+        // Голова енота (овал)
+        this.svg
+            .append("ellipse")
+            .attr("cx", this.centerX)
+            .attr("cy", this.centerY)
+            .attr("rx", 80)
+            .attr("ry", 70)
+            .style("fill", "gray");
+
+        // Уши енота (два прямоугольных треугольника)
+        this.svg
+            .append("polygon")
+            .attr(
+                "points",
+                `${this.centerX - 70},${this.centerY - 50} ${
+                    this.centerX - 30
+                },${this.centerY - 120} ${this.centerX - 30},${
+                    this.centerY - 50
+                }`
+            )
+            .style("fill", "gray");
+
+        this.svg
+            .append("polygon")
+            .attr(
+                "points",
+                `${this.centerX + 30},${this.centerY - 50} ${
+                    this.centerX + 30
+                },${this.centerY - 120} ${this.centerX + 70},${
+                    this.centerY - 50
+                }`
+            )
+            .style("fill", "gray");
+
+        // Глаза (два овала)
+        this.svg
+            .append("ellipse")
+            .attr("cx", this.centerX - 30)
+            .attr("cy", this.centerY - 10)
+            .attr("rx", 20)
+            .attr("ry", 30)
+            .style("fill", "white");
+
+        this.svg
+            .append("ellipse")
+            .attr("cx", this.centerX + 30)
+            .attr("cy", this.centerY - 10)
+            .attr("rx", 20)
+            .attr("ry", 30)
+            .style("fill", "white");
+
+        // Зрачки (два овала)
+        this.svg
+            .append("ellipse")
+            .attr("cx", this.centerX - 30)
+            .attr("cy", this.centerY - 10)
+            .attr("rx", 10)
+            .attr("ry", 15)
+            .style("fill", "black");
+
+        this.svg
+            .append("ellipse")
+            .attr("cx", this.centerX + 30)
+            .attr("cy", this.centerY - 10)
+            .attr("rx", 10)
+            .attr("ry", 15)
+            .style("fill", "black");
+
+        // Полосы вокруг глаз (два прямоугольника с радиусом закругления)
+        this.svg
+            .append("rect")
+            .attr("x", this.centerX - 60)
+            .attr("y", this.centerY - 30)
+            .attr("width", 60)
+            .attr("height", 40)
+            .attr("rx", 20)
+            .attr("ry", 20)
+            .style("fill", "black")
+            .style("opacity", 0.5);
+
+        this.svg
+            .append("rect")
+            .attr("x", this.centerX)
+            .attr("y", this.centerY - 30)
+            .attr("width", 60)
+            .attr("height", 40)
+            .attr("rx", 20)
+            .attr("ry", 20)
+            .style("fill", "black")
+            .style("opacity", 0.5);
+
+        // Нос (треугольник)
+        this.svg
+            .append("polygon")
+            .attr(
+                "points",
+                `${this.centerX - 5},${this.centerY + 10} ${this.centerX + 5},${
+                    this.centerY + 10
+                } ${this.centerX},${this.centerY + 30}`
+            )
+            .style("fill", "black");
+
+        // Рот (дуга)
+        this.svg
+            .append("path")
+            .attr(
+                "d",
+                `M${this.centerX - 20},${this.centerY + 30} Q${this.centerX},${
+                    this.centerY + 50
+                } ${this.centerX + 20},${this.centerY + 30}`
+            )
+            .style("fill", "none")
+            .style("stroke", "black")
+            .style("stroke-width", 2);
+
+        // Усики (линии)
+        var whiskerData = [
+            {
+                x1: this.centerX - 10,
+                y1: this.centerY + 15,
+                x2: this.centerX - 50,
+                y2: this.centerY + 5,
+            },
+            {
+                x1: this.centerX - 10,
+                y1: this.centerY + 20,
+                x2: this.centerX - 50,
+                y2: this.centerY + 20,
+            },
+            {
+                x1: this.centerX - 10,
+                y1: this.centerY + 25,
+                x2: this.centerX - 50,
+                y2: this.centerY + 35,
+            },
+            {
+                x1: this.centerX + 10,
+                y1: this.centerY + 15,
+                x2: this.centerX + 50,
+                y2: this.centerY + 5,
+            },
+            {
+                x1: this.centerX + 10,
+                y1: this.centerY + 20,
+                x2: this.centerX + 50,
+                y2: this.centerY + 20,
+            },
+            {
+                x1: this.centerX + 10,
+                y1: this.centerY + 25,
+                x2: this.centerX + 50,
+                y2: this.centerY + 35,
+            },
+        ];
+
+        whiskerData.forEach((d) => {
+            this.svg
+                .append("line")
+                .attr("x1", d.x1)
+                .attr("y1", d.y1)
+                .attr("x2", d.x2)
+                .attr("y2", d.y2)
+                .style("stroke", "black")
+                .style("stroke-width", 2);
+        });
+    }
+}
+
+class Fox extends Draw {
+    draw() {
+        // Голова лисы (овал)
+        this.svg
+            .append("ellipse")
+            .attr("cx", this.centerX)
+            .attr("cy", this.centerY)
+            .attr("rx", 80)
+            .attr("ry", 70)
+            .style("fill", "orange");
+
+        // Уши лисы (два треугольника)
+        this.svg
+            .append("polygon")
+            .attr(
+                "points",
+                `${this.centerX - 60},${this.centerY - 20} ${
+                    this.centerX - 20
+                },${this.centerY - 110} ${this.centerX - 20},${
+                    this.centerY - 20
+                }`
+            )
+            .style("fill", "orange");
+
+        this.svg
+            .append("polygon")
+            .attr(
+                "points",
+                `${this.centerX + 20},${this.centerY - 20} ${
+                    this.centerX + 20
+                },${this.centerY - 110} ${this.centerX + 60},${
+                    this.centerY - 20
+                }`
+            )
+            .style("fill", "orange");
+
+        // Внутренняя часть ушей (два треугольника)
+        this.svg
+            .append("polygon")
+            .attr(
+                "points",
+                `${this.centerX - 50},${this.centerY - 20} ${
+                    this.centerX - 20
+                },${this.centerY - 80} ${this.centerX - 20},${
+                    this.centerY - 20
+                }`
+            )
+            .style("fill", "white");
+
+        this.svg
+            .append("polygon")
+            .attr(
+                "points",
+                `${this.centerX + 20},${this.centerY - 20} ${
+                    this.centerX + 20
+                },${this.centerY - 80} ${this.centerX + 50},${
+                    this.centerY - 20
+                }`
+            )
+            .style("fill", "white");
+
+        // Глаза (два овала)
+        this.svg
+            .append("ellipse")
+            .attr("cx", this.centerX - 30)
+            .attr("cy", this.centerY)
+            .attr("rx", 15)
+            .attr("ry", 20)
+            .style("fill", "white");
+
+        this.svg
+            .append("ellipse")
+            .attr("cx", this.centerX + 30)
+            .attr("cy", this.centerY)
+            .attr("rx", 15)
+            .attr("ry", 20)
+            .style("fill", "white");
+
+        // Зрачки (два овала)
+        this.svg
+            .append("ellipse")
+            .attr("cx", this.centerX - 30)
+            .attr("cy", this.centerY)
+            .attr("rx", 7)
+            .attr("ry", 10)
+            .style("fill", "black");
+
+        this.svg
+            .append("ellipse")
+            .attr("cx", this.centerX + 30)
+            .attr("cy", this.centerY)
+            .attr("rx", 7)
+            .attr("ry", 10)
+            .style("fill", "black");
+
+        // Нос (треугольник)
+        this.svg
+            .append("polygon")
+            .attr(
+                "points",
+                `${this.centerX - 5},${this.centerY + 30} ${this.centerX + 5},${
+                    this.centerY + 30
+                } ${this.centerX},${this.centerY + 50}`
+            )
+            .style("fill", "black");
+
+        // Рот (дуга)
+        this.svg
+            .append("path")
+            .attr(
+                "d",
+                `M${this.centerX - 20},${this.centerY + 50} Q${this.centerX},${
+                    this.centerY + 70
+                } ${this.centerX + 20},${this.centerY + 50}`
+            )
+            .style("fill", "none")
+            .style("stroke", "black")
+            .style("stroke-width", 2);
+
+        // Усики (линии)
+        var whiskerData = [
+            {
+                x1: this.centerX - 10,
+                y1: this.centerY + 35,
+                x2: this.centerX - 50,
+                y2: this.centerY + 25,
+            },
+            {
+                x1: this.centerX - 10,
+                y1: this.centerY + 40,
+                x2: this.centerX - 50,
+                y2: this.centerY + 40,
+            },
+            {
+                x1: this.centerX - 10,
+                y1: this.centerY + 45,
+                x2: this.centerX - 50,
+                y2: this.centerY + 55,
+            },
+            {
+                x1: this.centerX + 10,
+                y1: this.centerY + 35,
+                x2: this.centerX + 50,
+                y2: this.centerY + 25,
+            },
+            {
+                x1: this.centerX + 10,
+                y1: this.centerY + 40,
+                x2: this.centerX + 50,
+                y2: this.centerY + 40,
+            },
+            {
+                x1: this.centerX + 10,
+                y1: this.centerY + 45,
+                x2: this.centerX + 50,
+                y2: this.centerY + 55,
+            },
+        ];
+
+        whiskerData.forEach((d) => {
+            this.svg
+                .append("line")
+                .attr("x1", d.x1)
+                .attr("y1", d.y1)
+                .attr("x2", d.x2)
+                .attr("y2", d.y2)
+                .style("stroke", "black")
+                .style("stroke-width", 2);
+        });
+    }
+}
+
+var point = {
+    x: 200,
+    y: 200,
+};
+
+svg.on("click", function (event) {
+    var [x, y] = d3.pointer(event); // Получаем координаты относительно SVG
+    console.log("Координаты клика относительно SVG: ", x, y);
+    point = { x: Math.round(x), y: Math.round(y) };
+
+    d3.select("p").text(`Точка размещения: {${point.x}; ${point.y}}`);
+});
+
+var lastFigure = null;
+
+d3.select("#draw").on("click", function () {
+    check = d3.select('input[name="figure"]:checked');
+
+    if (check.empty()) {
+        return 0;
+    } else {
+        var selectedValue = check.property("value");
+        var svgContainer = svg.append("g");
+
+        if (selectedValue === "fox") {
+            lastFigure = new Fox(svgContainer, point.x, point.y);
+        } else if (selectedValue === "raccoon") {
+            lastFigure = new Raccoon(svgContainer, point.x, point.y);
+        }
+
+        lastFigure.draw();
+    }
+});
+
+d3.select("#animate").on("click", function () {
+    if (
+        lastFigure != null &&
+        (lastFigure.constructor.name == "Raccoon" ||
+            lastFigure.constructor.name == "Fox")
+    )
+        lastFigure.animate(2000);
+});
+
+d3.select("#clear").on("click", function () {
+    svg.selectAll("g").remove();
+});
