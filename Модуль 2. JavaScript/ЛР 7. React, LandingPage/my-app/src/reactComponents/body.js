abb4d4794d0f758a6cdf0fe6a61f74abee67de94
VerkhovtsovDenis <verkhovtcov.do@dvfu.ru>
2024-06-05 02:42:41 +1000

    Новые компоненты добавлены для рендеринга

--- Модуль 2. JavaScript/ЛР 7. React, LandingPage/my-app/src/reactComponents/body.js
+++ Модуль 2. JavaScript/ЛР 7. React, LandingPage/my-app/src/reactComponents/body.js
@@ -2,14 +2,18 @@
 import Infrastructure from './infrastructure.js'
 import Slider from './slider.js'
 import GuessingGame from './guessingGame.js'
+import InputForm from './form.js';
+import Info from './info.js';
 
 function Body(props) {
     return (
         <div className="body">
-            <h1>ИННОВАЦИОННЫЙ НАУЧНО-ТЕХНОЛОГИЧЕСКИЙ ЦЕНТР «РУССКИЙ»</h1>
-            <Infrastructure objects={props.objects} map={props.map} />
-			<Slider listImages={props.listImages} />
-            <GuessingGame />
+            <h1>{props.listTitles.main}</h1>
+            <Infrastructure objects={props.objects} map={props.map} name={props.listTitles.infrastructure}/>
+			<Slider listImages={props.listImages} name={props.listTitles.slider}/>
+            <GuessingGame name={props.listTitles.guessingGame}/>
+            <InputForm arr={[]} helpP="1" name={props.listTitles.inputForm}/>
+            <Info name={props.listTitles.info} tableData={props.tableData}/>
 		</div>
     );
 }
